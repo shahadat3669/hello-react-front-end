ebf7a539248c3ffd771f1fa14af6b1cfbda0ff02
Shahadat Hossain <shahadat3669@gmail.com>
2023-05-06 21:45:28 +0600

    Add and configure React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return <div>Hi form app js</div>;
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route
+        path="/"
+        element={<h1>hi</h1>}
+      />
+    </Routes>
+  </Router>
+);
 
 export default App;
